04e852eef79092fa6ce82c0e9ecec83cdf0aacc9
UuBroot <81309724+UuBroot@users.noreply.github.com>
2024-12-03 23:19:09 +0100

    moved supported formats to the module file

--- system/main.py
+++ system/main.py
@@ -1,15 +1,10 @@
 import os
 from enum import Enum
 import filetype
-from system.modules.module_pillow import convert as convImg
 
-#######################
-## Supported Formats ##
-#######################
 ### Pillow ###
-supportedImageFormats = (
-    "png", "jpg", "jpeg", "bmp", "dds", "dib", "pcx", "ps", "eps", "gif", "apng", "jp2", "j2k", "jpc", "jpf", "jpx", "j2c", "icns", "ico", "im", "jfif", "jpe", "tif", "tiff", "pbm", "pgm", "ppm", "pnm", "pfm", "bw", "rgb", "rgba", "sgi", "tga", "icb", "vda", "vst", "webp"
-)
+from system.modules.module_pillow import convert as convImg
+from system.modules.module_pillow import formatSupported as imageFormatSupported  
 ### FFMPEG ###
 supportedVideoFormats = (
     "mp4"
--- system/modules/module_pillow.py
+++ system/modules/module_pillow.py
@@ -1,5 +1,11 @@
 from PIL import Image
 
-def convert(filepath, format):
+supportedFormats = (
+    "png", "jpg", "jpeg", "bmp", "dds", "dib", "pcx", "ps", "eps", "gif", "apng", "jp2", "j2k", "jpc", "jpf", "jpx", "j2c", "icns", "ico", "im", "jfif", "jpe", "tif", "tiff", "pbm", "pgm", "ppm", "pnm", "pfm", "bw", "rgb", "rgba", "sgi", "tga", "icb", "vda", "vst", "webp"
+)
+
+def convert(filepath: str, format: str):
     img_png = Image.open(filepath)
-    img_png.save("./export."+str(format))
+    img_png.save("./export."+str(format))    
+def formatSupported(format: str):
+    return supportedFormats.__contains__(format)
