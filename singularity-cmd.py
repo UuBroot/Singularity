a3a6574e08fa2d45985ebfa3d70ea5d9fc7ddbac
UuBroot <81309724+UuBroot@users.noreply.github.com>
2024-12-04 09:08:44 +0100

    added ability to both have video and audio codecs in cmd app

--- singularity-cmd.py
+++ singularity-cmd.py
@@ -2,7 +2,7 @@ import sys
 import system.main
 
 if len(sys.argv) < 3 or len(sys.argv) > 4:
-    print("Usage: python main.py <path to file> <path to output.format> <optional codec>")
+    print("Usage: python main.py <path to file> <path to output.format> <optional video codec/optional audio codec>")
     sys.exit(1)
 
 try:
