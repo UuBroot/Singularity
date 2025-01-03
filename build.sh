bbe8be2ebb3a8417d6775743b427b91476417761
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-01-03 23:55:49 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-nuitka --follow-imports --standalone --onefile --output-dir=export --include-plugin-directory=system --plugin-enable=pyqt6 singularity-qt.py d
+nuitka --follow-imports --standalone --onefile --output-dir=build --include-plugin-directory=system --plugin-enable=pyqt6 singularity-qt.py
