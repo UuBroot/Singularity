d33abb2dd4fcdefb7d62454497597227c8efc625
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-01-07 23:16:12 +0100

    added psd support to pillow module

--- system/modules/module_pillow.py
+++ system/modules/module_pillow.py
@@ -5,7 +5,7 @@ from global_vars import globals, FinishedType
 class Pillow(Module):
     def __init__(self):
         supportedFormats = (
-            "png", "jpg", "jpeg", "bmp", "dds", "dib", "pcx", "ps", "eps", "gif", "apng", "jp2", "j2k", "jpc", "jpf", "jpx", "j2c", "icns", "ico", "im", "jfif", "jpe", "tif", "tiff", "pbm", "pgm", "ppm", "pnm", "pfm", "bw", "rgb", "rgba", "sgi", "tga", "icb", "vda", "vst", "webp"
+            "png", "jpg", "jpeg", "bmp", "dds", "dib", "pcx", "ps", "eps", "gif", "apng", "jp2", "j2k", "jpc", "jpf", "jpx", "j2c", "icns", "ico", "im", "jfif", "jpe", "tif", "tiff", "pbm", "pgm", "ppm", "pnm", "pfm", "bw", "rgb", "rgba", "sgi", "tga", "icb", "vda", "vst", "webp", "psd"
         )
         super().__init__(supportedFormats)
 
