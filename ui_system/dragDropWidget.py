3132dbfbf2d1dceaeabfccfba9ff3f308b5671c5
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-01-07 10:26:06 +0100

    made some design changes in the dragDropWidget

--- ui_system/dragDropWidget.py
+++ ui_system/dragDropWidget.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
-from PySide6.QtCore import Signal
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
 
 class DragDropWidget(QWidget):
     signal = Signal(str)
@@ -10,18 +11,64 @@ class DragDropWidget(QWidget):
         self.initUI()
 
     def initUI(self):
-        layout = QVBoxLayout()
-        self.label = QLabel("Drag and drop a file here")
-        layout.addWidget(self.label)
-        self.setLayout(layout)
+        self.layout = QStackedLayout()
+        self.setLayout(self.layout)
+
+        ##guide lable
+        self.guideLabelBox = QWidget()
+        guideLabelBoxLayout = QHBoxLayout()
+        guidelabel = QLabel("Drag and drop a file here")
+        guidelabel.setAlignment(Qt.AlignCenter)  # Center the text
+        guideLabelBoxLayout.addWidget(guidelabel)
+        self.guideLabelBox.setLayout(guideLabelBoxLayout)
+
+        self.layout.addWidget(self.guideLabelBox)
+        
+        ##on drag screen
+        self.dragScreen = QWidget()
+        dragScreenLayout = QHBoxLayout()
+        
+        dragScreenLayout.addItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        
+        drag_icon = QLabel()
+        drag_icon.setPixmap(QIcon.fromTheme("document-open").pixmap(64, 64))
+        dragScreenLayout.addWidget(drag_icon)
+        
+        self.dragScreen.setLayout(dragScreenLayout)
+        self.layout.addWidget(self.dragScreen)
+        
+        dragScreenLayout.addItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        
         self.setAcceptDrops(True)
-        self.setStyleSheet("border: 10px solid #454547;border-radius:15px;padding: 20px;")
+        self.setStyleSheet("""
+                            background-color: #5C5C5C;
+                            border-radius:15px;
+                            padding: 20px;
+
+                           """)
 
     def dragEnterEvent(self, event):
+        self.layout.setCurrentWidget(self.dragScreen)
+        self.setStyleSheet("""
+                            background-color: #292929;
+                            border-radius:15px;
+                            padding: 20px;
+                            
+                           """)
+        
         if event.mimeData().hasUrls:
             event.accept()
         else:
             event.ignore()
+    
+    def dragLeaveEvent(self, event):
+        self.layout.setCurrentWidget(self.guidelabel)
+        self.setStyleSheet("""
+                            background-color: #5C5C5C;
+                            border-radius:15px;
+                            padding: 20px;
+                            
+                           """)
 
     def dropEvent(self, event):
         if event.mimeData().hasUrls():
