1681ef3bcd5af97a811359f54c1f8f9742eb7aa4
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-01-09 08:17:57 +0100

    added build scripts for all supported operating systems

---
+++ build.cmd
@@ -0,0 +1,1 @@
+nuitka --follow-imports --standalone --onefile --output-dir=build --include-plugin-directory=system --plugin-enable=pyside6 --assume-yes-for-downloads --msvc=latest singularity-qt.py
--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-nuitka --follow-imports --standalone --onefile --output-dir=build --include-plugin-directory=system --plugin-enable=pyqt6 singularity-qt.py
+OS=$(uname)
+if [ "$OS" = "Darwin" ]; then
+    echo "Running on macOS"
+    nuitka --follow-imports --standalone --macos-create-app-bundle --output-dir=build --include-plugin-directory=system --plugin-enable=pyside6 --assume-yes-for-downloads singularity-qt.py
+elif [ "$OS" = "Linux" ]; then
+    echo "Running on Linux"
+    nuitka --follow-imports --standalone --onefile --output-dir=build --include-plugin-directory=system --plugin-enable=pyside6 singularity-qt.py
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
